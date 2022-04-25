6780067b8bb987de3583e4d269afd02d9da54eac
jon <jonhel619@gmail.com>
2022-04-25 10:28:45 +0800

    finished changes to bbp

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 let playerScore = 0;
+let computerScore = 0;
 
-//result message
-const resWin = `You win!`;
-const resTie = `Tie!`;
-const resLose = `You lose!`;
-const resErr = "Invalid entry. Please try again."
-
-
-// sets computer's choice for the round
-function computerPlay() {
-    const randNum = Math.floor(Math.random() * 3);
-    if (randNum == 0) {
-        return "rock";
-    } else if (randNum == 1) {
-        return "paper";
-    } else return "scissors";
-}
-
-const computerSelection = computerPlay();
-
+//adds listener to all buttons
+let playerChoice = document.getElementsByClassName("button");
+for (var i = 0 ; i < playerChoice.length; i++) {
+    playerChoice[i].addEventListener('click', playRound); 
+ }
 
 //for results of gameplay
-function playRound(playerSelection, computerSelection) {
+function playRound(e, ) {
+    let playerSelection = e.target.alt;
+
+    // sets computer's choice for the round
+    function computerPlay() {
+        const randNum = Math.floor(Math.random() * 3);
+        if (randNum == 0) {
+            return "rock";
+        } else if (randNum == 1) {
+            return "paper";
+        } else return "scissors";
+    }
+
+    //game over overlay
+    function openOverlay() {
+        document.getElementById("gameover").style.display = "block";
+    }
+
+    let computerSelection = computerPlay();
+    
     if (playerSelection == computerSelection) {
-        return resTie;
+        document.getElementById("resultmess").innerHTML = "tie.";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return resLose;
+        computerScore++;
+        document.getElementById("resultmess").innerHTML = "You lost.";
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
-        return resWin;
+        document.getElementById("resultmess").innerHTML = "Yay! You win.";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
-        return resWin;
+        document.getElementById("resultmess").innerHTML = "Yay! You win.";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return resLose;
+        computerScore++;
+        document.getElementById("resultmess").innerHTML = "You lost.";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return resLose;
+        computerScore++;
+        document.getElementById("resultmess").innerHTML = "You lost.";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore++;
-        return resWin;
+        document.getElementById("resultmess").innerHTML = "Yay! You win.";
     } else {
-        return resErr;
+        document.getElementById("resultmess").innerHTML = "No.";
+    }
+    document.getElementById("pscore").innerHTML = playerScore;
+    document.getElementById("cscore").innerHTML = computerScore;
+    document.getElementById("pchoice").innerHTML = playerSelection.toUpperCase();
+    document.getElementById("cchoice").innerHTML = computerSelection.toUpperCase();
+
+    if (computerScore == 5) {
+        document.getElementById("result").innerHTML = "Good game.";
+        document.getElementById("overall").innerHTML = "Computer Won. :(";
+        openOverlay();
+    }
+
+    if (playerScore == 5) {
+        document.getElementById("result").innerHTML = "Good game.";
+        document.getElementById("overall").innerHTML = "You Won!";
+        document.getElementById("fwone").classList.add("before");
+        document.getElementById("fwtwo").classList.add("after");
+        openOverlay();
     }
   }
 
-
-
-//for 5 rounds of gameplay
-function game() {
-    let i;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = window.prompt("Rock, paper, or scissors?").toLowerCase();
-        console.log(playerSelection);
-        const computerSelection = computerPlay();
-        console.log(computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(playerScore);
-    }
+// resultmess color
+let mainpage = document.getElementById("main");
+mainpage.addEventListener('mousemove', runEvent);
+function runEvent(e) {
+    document.getElementById("resultmess").style.color = "rgb(40, "+e.offsetY+", "+e.offsetX+")";
 }
-
-game();
-
